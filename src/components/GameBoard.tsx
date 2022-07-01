980f8a39c2703624d33b58fdac2afa6547189708
Kamil Czyżewski <kamil.czyzewski96@gmail.com>
2022-07-01 14:57:10 +0200

    Working on main game logic

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,80 +1,166 @@
 import "../styles/gameBoard.css";
 import { numSquareSizes, iconSquareSizes } from "../data/Config";
 import { useEffect, useState } from "react";
+
 interface Props {
-    backToMenu: () => void;
-    theme: string,
-    size: string,
-    time: string
+  backToMenu: () => void;
+  theme: string;
+  size: string;
+  time: string;
+}
+
+interface Square {
+  square: string;
+  id: string;
+  selected: boolean;
 }
 
 const GameBoard: React.FC<Props> = ({ backToMenu, theme, time, size }) => {
-    const [gameSize, setGameSize] = useState<{
+  const [gameSize, setGameSize] = useState<
+    | {
         boardSize: number;
         squareSize: number;
-    } | undefined>()
-    const [gameBoard, setGameBoard] = useState<{
+      }
+    | undefined
+  >();
+  const [gameBoard, setGameBoard] = useState<
+    | {
         square: string;
         id: string;
-    }[] | undefined>()
-    const [minutes, setMinutes] = useState(parseInt(time[0]) - 1);
-    const [seconds, setSeconds] = useState(59);
+        selected: boolean;
+      }[]
+    | undefined
+  >();
+  const [minutes, setMinutes] = useState(parseInt(time[0]) - 1);
+  const [seconds, setSeconds] = useState(59);
+  const [squaresToCompare, setSquaresToCompare] = useState<Square[]>([]);
 
-    useEffect(() => {
-        let myInterval = setInterval(() => {
-            if (seconds > 0) {
-                setSeconds(seconds - 1);
-            }
-            if (seconds === 0) {
-                if (minutes === 0) {
-                    clearInterval(myInterval)
-                } else {
-                    setMinutes(minutes - 1);
-                    setSeconds(59);
-                }
-            }
-        }, 1000)
-        return () => {
-            clearInterval(myInterval);
-        };
-    });
-
-    useEffect(() => {
-        if (theme === "numbers") {
-            const selectedSize = numSquareSizes.find(gameSize => gameSize.boardSize === parseInt(size))
-            const selectedBoard = selectedSize?.board
-
-            setGameSize(selectedSize)
-            setGameBoard(selectedBoard?.sort(() => 0.5 - Math.random()))
-
-        } else if (theme === "icons") {
-            const selectedSize = iconSquareSizes.find(gameSize => gameSize.boardSize === parseInt(size))
-            const selectedBoard = selectedSize?.board
-
-            setGameSize(selectedSize)
-            setGameBoard(selectedBoard?.sort(() => 0.5 - Math.random()))
+  useEffect(() => {
+    let myInterval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(myInterval);
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
         }
-    }, [])
+      }
+    }, 1000);
+    return () => {
+      clearInterval(myInterval);
+    };
+  });
 
-    return (
-        <div className="gameBoard">
-            <div className="gameBoardOptions">
-                <div className="countdown">{seconds > 9 ? <h3>0{minutes}:{seconds}</h3> : <h3>0{minutes}:0{seconds}</h3>}</div>
-                <div className="buttons">
-                    <button className="restartButton">Restart</button>
-                    <button className="newGameButton" onClick={() => backToMenu()}>New game</button>
-                </div>
-            </div>
+  useEffect(() => {
+    if (theme === "numbers") {
+      const selectedSize = numSquareSizes.find(
+        (gameSize) => gameSize.boardSize === parseInt(size)
+      );
+      const selectedBoard = selectedSize?.board;
 
-            <div style={{ gridTemplate: `repeat(${gameSize?.boardSize}, 1fr) / repeat(${gameSize?.boardSize}, 1fr)` }} className="game">
-                {theme === "icons" ? gameBoard?.map(square => (
-                    <div key={Math.random()} style={{ width: gameSize?.squareSize, height: gameSize?.squareSize }} id="square"><i className={square.square}></i></div>
-                )) : gameBoard?.map(square => (
-                    <div key={Math.random()} style={{ width: gameSize?.squareSize, height: gameSize?.squareSize }} id="square"><p>{square.square}</p></div>
-                ))}
-            </div>
+      setGameSize(selectedSize);
+      setGameBoard(selectedBoard?.sort(() => 0.5 - Math.random()));
+    } else if (theme === "icons") {
+      const selectedSize = iconSquareSizes.find(
+        (gameSize) => gameSize.boardSize === parseInt(size)
+      );
+      const selectedBoard = selectedSize?.board;
+
+      setGameSize(selectedSize);
+      setGameBoard(selectedBoard?.sort(() => 0.5 - Math.random()));
+    }
+  }, []);
+
+  useEffect(() => {
+    // If EVERY square value in a gameBoard has selected === true - set game end.
+  }, [gameBoard]);
+
+  const selectSquare = (id: string) => {
+    // This function shows what's on "the other side" of a square. And now I can unlimitly check every square.
+    const selectedSquare = gameBoard?.map((square) => {
+      if (square.id === id) {
+        return { ...square, selected: true };
+      }
+      return square;
+    });
+    setGameBoard(selectedSquare);
+  }; // Now I need some piece of state which accepts only 2 values. 2 squares and compares them. If they don't match - I need to find them in gameBoard state and set them all to be non-selected. If they do match - their SELECTED property stays on true and they gain some new color.
+
+  const addSquaresToCompare = (square: {
+    square: string;
+    id: string;
+    selected: boolean;
+  }) => {
+    if (squaresToCompare.length < 2) {
+      setSquaresToCompare((squares) => [...squares, square]); // If there are less than 2 - add squares to compare.
+    } else {
+      console.log("CHECKING FUNCTION"); // If length exceeds 2 - run function that will chec if they're equal.
+    }
+  };
+
+  return (
+    <div className="gameBoard">
+      <div className="gameBoardOptions">
+        <div className="countdown">
+          {seconds > 9 ? (
+            <h3>
+              0{minutes}:{seconds}
+            </h3>
+          ) : (
+            <h3>
+              0{minutes}:0{seconds}
+            </h3>
+          )}
         </div>
-    )
-}
+        <div className="buttons">
+          <button className="restartButton">Restart</button>
+          <button className="newGameButton" onClick={() => backToMenu()}>
+            New game
+          </button>
+        </div>
+      </div>
 
-export default GameBoard
+      <div
+        style={{
+          gridTemplate: `repeat(${gameSize?.boardSize}, 1fr) / repeat(${gameSize?.boardSize}, 1fr)`,
+        }}
+        className="game"
+      >
+        {theme === "icons"
+          ? gameBoard?.map((square) => (
+              <div
+                key={Math.random()}
+                style={{
+                  width: gameSize?.squareSize,
+                  height: gameSize?.squareSize,
+                }}
+                id="square"
+              >
+                <i className={square.square}></i>
+              </div>
+            ))
+          : gameBoard?.map((square) => (
+              <div
+                onClick={() => {
+                  selectSquare(square.id);
+                  addSquaresToCompare(square);
+                }}
+                key={Math.random()}
+                style={{
+                  width: gameSize?.squareSize,
+                  height: gameSize?.squareSize,
+                }}
+                id="square"
+              >
+                <p>{square.selected ? square.square : null}</p>
+              </div>
+            ))}
+      </div>
+    </div>
+  );
+};
+
+export default GameBoard;
